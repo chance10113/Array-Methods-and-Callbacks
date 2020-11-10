98cf11d04f8bd7c0c5eb4002cdc510cafb9d0cd2
Chance Swartz <chanceswartz@gmail.com>
2020-11-10 17:43:48 -0600

    Almost finished task 3

--- index.js
+++ index.js
@@ -3,40 +3,58 @@ import { fifaData } from './fifa.js';
 // ⚽️ M  V P ⚽️ //
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Investigate the data above. Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-
-//(a) Home Team name for 2014 world cup final
-
-//(b) Away Team name for 2014 world cup final
-
-//(c) Home Team goals for 2014 world cup final
-
-//(d) Away Team goals for 2014 world cup final
-
-//(e) Winner of 2014 world cup final */
-
+Investigate the data above. Practice accessing data by console.log-ing the following pieces of data. 
+Note, you may want to filter the data first 😉*/
+// const finals2014 = fifaData.filter(function(item){
+//     return item.Year === 2014 && item.Stage === 'Final'
+// })
+// console.log(finals2014);
+// //(a) Home Team name for 2014 world cup final
+// console.log('Task 1 a:', finals2014[0]['Home Team Name']);
+// //(b) Away Team name for 2014 world cup final
+// console.log('Task 1 b :', finals2014[0]['Away Team Name']);
+// //(c) Home Team goals for 2014 world cup final
+// console.log('Task 1 c:', finals2014[0]['Home Team Goals']);
+// //(d) Away Team goals for 2014 world cup final
+// console.log('Task 1 d:', finals2014[0]['Away Team Goals']);
+// //(e) Winner of 2014 world cup final */
+// console.log('Task 1 e :', finals2014[0]['Win conditions'])
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
-Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
+Create a function called  getFinals that takes `data` as an argument 
+and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+    const finals = data.filter(function(data){
+        return data['Stage'] === 'Final'
+    }); return finals
 }
 
-
+// console.log(getFinals(fifaData))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
+Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, 
+and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(callback) {
+    const years = [];
+
+     callback.forEach(function(fifaData){
+        years.push([item.Year])
+        }); return years 
 }
+// const years = fifaData.map(function(item){
+//     return CB['Year']
+//     }); return years 
+// }
 
-
+console.log('getYears(CB): ', getYears(getFinals));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
+Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` 
+and determine the winner (home or away) of each `finals` game.
+ Return the name of all winning countries in an array called `winners` */ 
 
 function getWinners(/* code here */) {
     /* code here */
@@ -46,7 +64,8 @@ function getWinners(/* code here */) {
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
-Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
+Implement a higher-order function called `getWinnersByYear` that accepts the following parameters 
+and returns a set of strings "In {year}, {country} won the world cup!" 
 
 Parameters: 
  * callback function getWinners
@@ -58,7 +77,9 @@ function getWinnersByYear(/* code here */) {
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
+Write a function called `getAverageGoals` that accepts a parameter `data` 
+and returns the the average number of home team goals and away team goals scored per match 
+(Hint: use .reduce and do this in 2 steps) */
 
 function getAverageGoals(/* code here */) {
    /* code here */
@@ -72,7 +93,8 @@ function getAverageGoals(/* code here */) {
 /// 🥅 STRETCH 🥅 ///
 
 /* 💪💪💪💪💪 Stretch 1: 💪💪💪💪💪 
-Create a function called `getCountryWins` that takes the parameters `data` and `team initials` and returns the number of world cup wins that country has had. 
+Create a function called `getCountryWins` that takes the parameters `data` and `team initials` 
+and returns the number of world cup wins that country has had. 
 
 Hint: Investigate your data to find "team initials"!
 Hint: use `.reduce` */
@@ -86,7 +108,8 @@ function getCountryWins(/* code here */) {
 
 
 /* 💪💪💪💪💪 Stretch 2: 💪💪💪💪💪 
-Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
+Write a function called getGoals() that accepts a parameter `data` 
+and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
 
 function getGoals(/* code here */) {
 
@@ -96,7 +119,9 @@ function getGoals(/* code here */) {
 
 
 /* 💪💪💪💪💪 Stretch 3: 💪💪💪💪💪
-Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
+Write a function called badDefense() that accepts a parameter `data` 
+and calculates the team with the most goals scored against them per appearance 
+(average goals against) in the World Cup finals */
 
 function badDefense(/* code here */) {
 
@@ -105,7 +130,8 @@ function badDefense(/* code here */) {
 }
 
 
-/* If you still have time, use the space below to work on any stretch goals of your chosing as listed in the README file. */
+/* If you still have time, use the space below to work on any stretch goals of your chosing 
+as listed in the README file. */
 
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
